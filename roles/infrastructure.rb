65eba2f27035f6c0aa68f010ec8a68ab5aa9e9c7
c01o <melty.antarcticite@gmail.com>
2015-09-21 23:25:39 +0900

    fix: problems about recipe including order

--- roles/infrastructure.rb
+++ roles/infrastructure.rb
@@ -5,6 +5,8 @@ include_recipe "./makeuser.rb"
 include_recipe "./enable_sudoPATH.rb"
 include_recipe "./apt_update.rb"
 # user settings
+include_recipe "../cookbooks/git/default.rb"
+include_recipe "../cookbooks/dotfiles/default.rb"
 include_recipe "../cookbooks/zsh/default.rb"
 
 # individual packages
@@ -22,11 +24,9 @@ make_tmp_dir TMP_ROOT do
   root_flag true
 end
 
-include_recipe "../cookbooks/git/default.rb"
 include_recipe "../cookbooks/bundler/default.rb"
 include_recipe "../cookbooks/golang/default.rb"
 include_recipe "../cookbooks/hub/default.rb"
 include_recipe "../cookbooks/vim/default.rb"
-include_recipe "../cookbooks/dotfiles/default.rb"
 
 cleanup_dir TMP_ROOT
