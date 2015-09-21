a82ec131e9a3f120fa45049708f6c042f5b7b1f4
c01o <melty.antarcticite@gmail.com>
2015-09-21 23:16:31 +0900

    add: install and build vimproc before running NeoBundleInstall

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -13,6 +13,12 @@ git "/home/#{node['username']}/.vim/bundle/vimproc.vim" do
   user node['username']
 end
 
+execute "build vimproc" do
+  command "make"
+  user node['username']
+  cwd "/home/#{node['username']}/.vim/bundle/vimproc.vim"
+end
+
 # run :NeoBundleInstall automatically after .vimrc placed
 execute "install vim plugins" do
   command %Q(vim -u ~/.vimrc -i NONE -c "try | NeoBundleUpdate! | finally | q! | endtry" -e -s -V1 -V9neobundle.log)
