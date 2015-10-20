e3bfea1de1a0b1f8298c00c0a0b648d8b2fc703c
c01o <melty.antarcticite@gmail.com>
2015-10-21 00:19:05 +0900

    add: settigs about golang binary

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -1,17 +1,16 @@
-GO_VERSION = "1.5"
-GO_PACKAGE = "go#{GO_VERSION}.linux-amd64.tar.gz"
-GO_BINARY_URL = "https://storage.googleapis.com/golang/#{GO_PACKAGE}"
-
 make_tmp_dir "go"
 TMP_PATH = "#{node['TMP_ROOT']}/go"
 
-download_to_directory GO_BINARY_URL do
+package_name = node['golang']['package_name'] % {version: node['golang']['version'], platform: node['golang']['platform']}
+binary_url = node['golang']['binary_url'] % {package_name: package_name}
+
+download_to_directory binary_url do
   destdir TMP_PATH
   user node['username']
 end
 
 execute "extract go_binary" do
-  command "tar -C /usr/local -xzf #{GO_PACKAGE}"
+  command "tar -C /usr/local -xzf #{package_name}"
   cwd TMP_PATH
   not_if "test -d /usr/local/go"
 end
