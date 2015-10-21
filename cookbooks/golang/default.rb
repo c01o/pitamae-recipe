19b05248b704786de34eb0eacb830d9039d9b77b
c01o <melty.antarcticite@gmail.com>
2015-10-22 00:44:42 +0900

    temp fix: workaround for not_if in definition

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -4,10 +4,11 @@ TMP_PATH = "#{node['TMP_ROOT']}/go"
 package_name = node['golang']['package_name'] % {version: node['golang']['version'], platform: node['golang']['platform']}
 binary_url = node['golang']['binary_url'] % {package_name: package_name}
 
-download_to_directory binary_url do
-  destdir TMP_PATH
-  user node['username']
-  not_if "test -d /usr/local/go"
+unless run_command("test -d /usr/local/go").exit_status == 0
+  download_to_directory binary_url do
+    destdir TMP_PATH
+    user node['username']
+  end
 end
 
 execute "extract go_binary" do
