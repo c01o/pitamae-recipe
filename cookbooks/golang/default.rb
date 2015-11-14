68afd35d810dcf9ab8d64c177ad33e819d0992ee
c01o <melty.antarcticite@gmail.com>
2015-11-15 06:27:30 +0900

    fix: avoid to raise error on golang that stops all provisioning

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -3,8 +3,9 @@ TMP_PATH = "#{node['TMP_ROOT']}/go"
 
 package_name = node['golang']['package_name'] % {version: node['golang']['version'], platform: node['golang']['platform']}
 binary_url = node['golang']['binary_url'] % {package_name: package_name}
+go_installed = run_command("test -d /usr/local/go", error: false).exit_status == 0
 
-unless run_command("test -d /usr/local/go").exit_status == 0
+unless go_installed
   download_to_directory binary_url do
     destdir TMP_PATH
     user node['username']
@@ -14,7 +15,7 @@ end
 execute "extract go_binary" do
   command "tar -C /usr/local -xzf #{package_name}"
   cwd TMP_PATH
-  not_if "test -d /usr/local/go"
+  not_if go_installed
 end
 
 add_PATH_with_zshenv "/usr/local/go/bin"
